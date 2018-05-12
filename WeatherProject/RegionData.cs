f4e676ab639934a58d56b49f08c951a1d08fc9cd
Orumiya <kasza.eszter.katalin@gmail.com>
2018-05-12 13:37:57 +0200

    Add project files.

---
+++ WeatherProject/RegionData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherProject
+{
+    class RegionData
+    {
+        private string regionName;
+
+        public string RegionName
+        {
+            get { return regionName; }
+            set { regionName = value; }
+        }
+
+        private double avgRainfall;
+
+        public double AvgRainfall
+        {
+            get { return avgRainfall; }
+            set { avgRainfall = value; }
+        }
+
+        public double MinTemperature { get; set; }
+
+        public double MaxTemperature { get; set; }
+
+    }
+}
---
+++ WeatherProject/XMLProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WeatherProject
+{
+    class XMLProcessor
+    {
+        XDocument xdoc;
+        public XMLProcessor(XDocument xdoc)
+        {
+            this.xdoc = xdoc;
+        }
+
+        public List<RegionData> GetRegionData()
+        {
+            List<RegionData> regionDataList = new List<RegionData>();
+            var q = xdoc.Element("weatherdata").Elements("datapoint").GroupBy(e => e.Element("location").Value);
+            IEnumerable<RegionData> d = xdoc.Element("weatherdata").Elements("datapoint").GroupBy(e => e.Element("location").Value)
+                .Select(x => new RegionData
+                {
+                    RegionName = x.Key,
+                    //AvgRainfall = x.Select(e => e.Elements("rainfall").Average(f => double.Parse(f.Value)))
+                });
+
+
+            //List<Datapoint> datapointList = new List<Datapoint>();
+            //var q = xdoc.Element("weatherdata").Elements("datapoint").GroupBy(e => e.Element("location").Value);
+            //IEnumerable<Datapoint> d = xdoc.Element("weatherdata").Elements("datapoint").GroupBy(e => e.Element("location").Value)
+            //    .Select(x => new Datapoint
+            //    {
+            //        RegionName = x.Key,
+            //        AvgRainfall = x.Select(e => e.Elements("rainfall").Average(f => double.Parse(f.Value)))
+            //    });
+
+
+            foreach (var item in d)
+            {
+                Console.WriteLine(item.RegionName);
+            }
+
+            var avgRain = q.Select(e => e.Elements("rainfall").Average(f => double.Parse(f.Value)));
+
+            var minTemp = q.Select(e => e.Elements("temperature").Min(f => double.Parse(f.Value)));
+            var maxTemp = q.Select(e => e.Elements("temperature").Max(f => double.Parse(f.Value)));
+            
+
+            foreach (var item in d)
+            {
+               
+               
+                );
+                Console.WriteLine(regionDataList.Last().RegionName + " : " + regionDataList.Last().AvgRainfall);
+            }
+            
+            return regionDataList;
+        }
+    }
+}
